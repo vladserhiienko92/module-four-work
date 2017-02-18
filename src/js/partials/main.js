7cbbd57c474cb1037702e565e6472808ffc58bf7
Vlad Serhiienko <vladserhiienko92@gmail.com>
2017-02-18 20:01:45 +0200

    push

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,5 +1,40 @@
 $(document).ready(function() {
 
-    
+    $(document).on("click", ".mobile-icon", function() {
+        $(".menu").slideToggle();
+        if ($(".mobile-icon").hasClass("active")) {
+            $(".mobile-icon").removeClass("active");
+        } else {
+            $(".mobile-icon").addClass("active");
+        }
+    });
+
+    $(".test-slider").slick({
+        "arrows": true,
+        "responsive": [
+            {
+              breakpoint: 992,
+              settings: {
+                "arrows": false
+            }
+            }
+        ]
+    });
+
+    $(document).on("click",".menu-item a, .header-button a, portfolio-button", function(e) {
+        e.preventDefault();
+        var id  = $(this).attr('href'),
+        var top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+    $(document).on("click",".portfolio-button", function(e) {
+        e.preventDefault();
+        var id  = $(this).attr('href'),
+        var top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+    $('#contact-form').validator();
 
 });
